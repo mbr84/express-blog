08d63522039a1184c476f7783423fc78a3c67680
mbr84 <matthew.bramfeld@gmail.com>
2016-10-17 12:34:56 -0700

    splash

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.sendfile('./views/index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendfile('./views/about.html');
+});
+
+app.get('/article', (req, res) => {
+  res.sendfile('./views/article.html');
+});
+
 app.listen(3000);
